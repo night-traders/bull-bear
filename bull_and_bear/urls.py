c38f07117cfc4c0f23ec40c2da892eac9d96cb9c
ticochuck <chucklivilla@gmail.com>
2020-11-03 07:32:52 -0800

    created stock model, view for unique stocks per user

--- bull_and_bear/urls.py
+++ bull_and_bear/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 
-from .views import (about, home)
+from .views import about, home, WatchlistView
 
 urlpatterns = [
     path('', home, name='home'),
     path('about/', about, name='about'),
+    path('watchlist/', WatchlistView.as_view(), name='watchlist'),
 ]
--- bull_and_bear/views.py
+++ bull_and_bear/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ValidationError
 from django.shortcuts import redirect, render
+from django.views.generic import DetailView, ListView, TemplateView
+
+from .models import Stock_ID
 
 
 def home(request):
@@ -19,3 +22,18 @@ def about(request):
     }
 
     return render(request, 'bull_and_bear/about.html', context)
+
+
+class WatchlistView(ListView):
+    template_name = 'bull_and_bear/watchlist.html'
+    model = Stock_ID
+
+# def watchlist(request):
+
+#     context = {
+#         'title': 'Watchlist'
+#     }
+
+#     my_stocks = Stock_ID.objects.get
+
+#     return render(request, 'bull_and_bear/watchlist.html', context)
