3e59d1c7c5573e478bb15f8aecd3b8d94b0c37d6
ticochuck <chucklivilla@gmail.com>
2020-11-03 09:52:17 -0800

    search form added, stocks per user displayed on watchlist

--- bull_and_bear/urls.py
+++ bull_and_bear/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 
-from .views import about, home, WatchlistView
+from .views import about, home, watchlist
+# WatchlistView
 
 urlpatterns = [
     path('', home, name='home'),
     path('about/', about, name='about'),
-    path('watchlist/', WatchlistView.as_view(), name='watchlist'),
+    path('watchlist/', watchlist, name='watchlist'),
+    # path('watchlist/', WatchlistView.as_view(), name='watchlist'),
 ]
--- bull_and_bear/views.py
+++ bull_and_bear/views.py
@@ -3,6 +3,7 @@ from django.core.exceptions import ValidationError
 from django.shortcuts import redirect, render
 from django.views.generic import DetailView, ListView, TemplateView
 
+from .forms import SearchStockForm
 from .models import Stock_ID
 
 
@@ -24,16 +25,50 @@ def about(request):
     return render(request, 'bull_and_bear/about.html', context)
 
 
-class WatchlistView(ListView):
-    template_name = 'bull_and_bear/watchlist.html'
-    model = Stock_ID
+# class WatchlistView(ListView):
+#     template_name = 'bull_and_bear/watchlist.html'
+#     model = Stock_ID
 
-# def watchlist(request):
+@login_required
+def watchlist(request):
 
+    if request.method == 'POST':
+        form = SearchStockForm(request.POST)
+
+    
+        if form.is_valid():
+            user = request.user
+            stock_ticker = request.POST.get('stock_ticker')
+            company_name = request.POST.get('company_name')
+
+            new_stock = Stock_ID(
+                user=user,
+                stock_ticker=stock_ticker,
+                company_name=company_name,
+            )
+            new_stock.save()
+                
+            return redirect('watchlist')
+
+        
+    else:
+        form = SearchStockForm()
+
+    my_stocks = Stock_ID.objects.all()
+
+    print(my_stocks)
+
+    context = {
+        'title': 'Watchlist',
+        'form': form,
+        'stocks': my_stocks,
+    }
+
+    return render(request, 'bull_and_bear/watchlist.html', context)
+
+# def results(request):
+
+#     stocks = Stock_ID.objects.get.all()
 #     context = {
-#         'title': 'Watchlist'
+#         'title',: ''
 #     }
-
-#     my_stocks = Stock_ID.objects.get
-
-#     return render(request, 'bull_and_bear/watchlist.html', context)
