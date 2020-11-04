81906b035d3680d9017a94bd87d3486957fe9282
Adam Owada <adamowada@gmail.com>
2020-11-03 19:33:09 -0800

    start work on model

--- bull_and_bear/models.py
+++ bull_and_bear/models.py
@@ -10,3 +10,12 @@ class Stock_ID(models.Model):
     def __str__(self):
         return f'{self.stock_ticker}, {self.company_name}'
 
+
+class Saved_Predictions(models.Model):
+    stock_ticker = models.CharField(max_length=5)
+    date_predicted = models.DateField(auto_now_add=True)
+    img = models.TextField()
+
+    def __str__(self):
+        return f'{self.stock_ticker}, {self.date_predicted}'
+
