32cc25145c8d37fb12ad4e573a282d6305ac3285
ticochuck <chucklivilla@gmail.com>
2020-11-04 21:11:16 -0800

    adding tests

--- bull_and_bear/tests.py
+++ bull_and_bear/tests.py
@@ -1,3 +1,7 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Stock_ID, Saved_Predictions
+
+
