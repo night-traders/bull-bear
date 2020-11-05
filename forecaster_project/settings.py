9ad12ecbc07a93f2ee2655eb04c82251ebf3e2c5
Roman Sydoruk <sydoruk89@gmail.com>
2020-11-05 01:41:27 -0800

    middleware

--- forecaster_project/settings.py
+++ forecaster_project/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
-import django_heroku
 from pathlib import Path
 
 import environ
@@ -60,6 +59,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
